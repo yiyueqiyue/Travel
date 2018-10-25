ca4e7b3c9ee9298ce875c5a871b6b832e89f35bd
yiyueqiyue <2249469670@qq.com>
2018-10-25 17:35:33 +0800

    travel

---
+++ Travel/src/pages/home/components/Swiper.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption" v-if="list.length">
+      <swiper-slide v-for="item of list" :key="item.id">
+        <img  class="swiper-img" :src="item.imgUrl" alt="">
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "HomeSwiper",
+    props: {
+      list: Array
+    },
+    data () {
+      return {
+        swiperOption: {
+          pagination: {
+            el: '.swiper-pagination'
+          },
+          loop: true,
+          autoplay: true,
+          speed: 1000
+        }
+      }
+    },
+    computed: {
+      showSwiper () {
+        return this.list.length
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus">
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: red !important
+  .wrapper
+    width: 100%
+    height: 0
+    overflow: hidden
+    padding-bottom: 31.25%
+    background: #ccc
+    .swiper-img
+      width: 100%
+</style>
